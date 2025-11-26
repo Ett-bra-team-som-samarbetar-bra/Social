db3c369f83ab850f17329c7f71a1ed9cca48c61c
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-11-26 16:38:52 +0100

    Added test for authservice aswell as refactored the passwordhashing into a helper instead of being in authservice to make it mockable with Moq

--- social-backend.tests/Services/AuthService/AuthServiceTests.cs
+++ social-backend.tests/Services/AuthService/AuthServiceTests.cs
@@ -1,5 +1,219 @@
+using social_backend.tests.Data;
+using SocialBackend.Data;
+using SocialBackend.Models;
+using SocialBackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Session;
+using Moq;
+using System.Drawing;
+using Microsoft.AspNetCore.Identity;
 namespace social_backend.tests;
 
-public class AusthServiceTest
+public class AuthServiceTest : TestBase
 {
+    private readonly IAuthService _authService;
+    private readonly DefaultHttpContext _httpContext;
+    private readonly TestSession _session;
+    private readonly Mock<IPasswordHelper> _mockHelper;
+    public AuthServiceTest()
+    {
+        _mockHelper = new Mock<IPasswordHelper>();
+        _authService = new AuthService(Context, _mockHelper.Object);
+
+        _httpContext = new DefaultHttpContext();
+        _session = new TestSession();
+        _httpContext.Features.Set<ISessionFeature>(new SessionFeature { Session = _session });
+    }
+
+    protected override void SeedData()
+    {
+        Context.Users.Add(new User { Id = 1, Username = "Alfred", Email = "Alfred@microsoft.com", PasswordHash = "Hej123" });
+        Context.Users.Add(new User { Id = 2, Username = "Manfred", Email = "Manfred@microsoft.com", PasswordHash = "123Lösen" });
+        Context.Users.Add(new User { Id = 3, Username = "Fredrik", Email = "Fredrik@microsoft.com", PasswordHash = "123456" });
+
+        Context.SaveChanges();
+    }
+
+
+    [Fact]
+    public void CreateUser_ShouldCreateANewUser()
+    {
+        //Arrange
+        string userName = "Nisse";
+        string email = "Nisse@manpower.se";
+        string password = "Password";
+        RegisterRequest newUser = new RegisterRequest
+        {
+            Username = userName,
+            Email = email,
+            Password = password
+        };
+        var passwordHash = newUser.Password;
+
+        //Act
+        var result = _authService.CreateUser(newUser, passwordHash);
+
+        //Assert
+        Assert.Equal(userName, result.Username);
+        Assert.IsType<User>(result);
+    }
+
+    [Fact]
+    public async Task DoesUserExist_ShouldReturnTrueWhenUserExists()
+    {
+        //Arrange
+        var username = "Alfred";
+
+        //Act
+        var result = await _authService.DoesUserExist(username);
+
+        //Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DoesUserExist_ShouldReturnFalseWhenUserDoesntExist()
+    {
+        //Arrange
+        var username = "Niklas";
+
+        //Act
+        var result = await _authService.DoesUserExist(username);
+
+        //Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void SetUserSession_ShouldStoreUserId()
+    {
+        //Arrange
+        var user = new User { Id = 4, Username = "Emil", Email = "Emil@microsoft.com", PasswordHash = "Dansbandsveckanimalung2022" };
+        var expected = 4;
+
+
+        //Act
+        _authService.SetUserSession(user, _httpContext);
+
+        //Assert
+        var actual = _httpContext.Session.GetInt32("UserId");
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Logout_ShouldRemoveStoredUserId()
+    {
+        //Arrange
+        _httpContext.Session.SetInt32("UserId", 1337);
+
+
+        //Act
+        _authService.Logout(_httpContext);
+
+        //Assert
+        Assert.False(_httpContext.Session.TryGetValue("UserId", out _));
+        Assert.Empty(_session.Keys);
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnUserWHenSuccesfullyLogsIn()
+    {
+        //Arrange
+        var request = new LoginRequest
+        {
+            Username = "Alfred",
+            Password = "Hej123"
+        };
+        _mockHelper
+        .Setup(h => h.IsPasswordVerified(
+            It.IsAny<string>(),
+            It.IsAny<string>()))
+        .Returns((string password, string hash) => password == hash);
+
+        //Act
+        var result = await _authService.Login(request, _httpContext);
+
+        //Assert
+        Assert.Equal(request.Username, result.Username);
+    }
+
+    [Fact]
+    public async Task Login_ShouldThrowWhenUserDoesNotExist()
+    {
+        var request = new LoginRequest
+        {
+            Username = "Plåtniklas",
+            Password = "Grodan Boll"
+        };
+
+        await Assert.ThrowsAsync<Exception>(() => _authService.Login(request, _httpContext));
+    }
+
+    [Fact]
+    public async Task Login_ShouldThrowWhenPasswordIsIncorrect()
+    {
+        var request = new LoginRequest
+        {
+            Username = "Alfred",
+            Password = "Tjuvar"
+        };
+        _mockHelper
+        .Setup(h => h.IsPasswordVerified(
+            It.IsAny<string>(),
+            It.IsAny<string>()))
+        .Returns((string password, string hash) => password == hash);
+
+        await Assert.ThrowsAsync<Exception>(() => _authService.Login(request, _httpContext));
+    }
+
+    [Fact]
+    public async Task RegisterAsync_ShouldAddUserToTheDataBaseIfUserDoesNotExist()
+    {
+        //Arrange
+        var newUser = new RegisterRequest
+        {
+            Username = "Pelle",
+            Email = "Pelle@Microsoft.com",
+            Password = "Hemligtsomfän"
+        };
+        _mockHelper.Setup(h => h.HashPassword(It.IsAny<string>())).Returns((string passwordHash) => passwordHash);
+        var expectedUserCount = Context.Users.Count() + 1;
+
+        //Act
+        await _authService.RegisterAsync(newUser);
+        var actualUserCount = Context.Users.Count();
+
+        //Assert
+        Assert.Equal(expectedUserCount, actualUserCount);
+    }
+    [Fact]
+    public async Task RegisterAsync_ShouldThrowWhenUserExists()
+    {
+        //Arrange
+        var newUser = new RegisterRequest
+        {
+            Username = "Alfred",
+            Email = "Alfred@Microsoft.com",
+            Password = "Hemligtsomfän"
+        };
+        _mockHelper.Setup(h => h.HashPassword(It.IsAny<string>())).Returns((string passwordHash) => passwordHash);
+
+        //Assert
+        await Assert.ThrowsAsync<Exception>(() => _authService.RegisterAsync(newUser));
+    }
 }
+
+/*
+public async Task RegisterAsync(RegisterRequest request)
+    {
+        if (await DoesUserExists(request.Username))
+            throw new Exception("Username already exists");
+
+        var passwordHash = HashPassword(request.Password);
+        var user = CreateUser(request, passwordHash);
+
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync();
+    }
+*/
