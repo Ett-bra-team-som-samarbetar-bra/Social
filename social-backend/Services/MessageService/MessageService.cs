728690dfe56732a398a162c057673d4e8484b1d3
jockeBjers <joakim.bjerselius@hotmail.com>
2025-11-26 14:56:34 +0100

    add check in sendMessage service to check for empty messages

--- social-backend/Services/MessageService/MessageService.cs
+++ social-backend/Services/MessageService/MessageService.cs
@@ -2,12 +2,21 @@ public class MessageService(IDatabaseContext context) : IMessageService
 {
     private readonly IDatabaseContext _context = context;
 
+    // remove later and use UserService and then mock dependency
+    public async Task<User> FindUserOrThrowAsync(int userId, string paramName)
+    {
+        return await _context.Users.FindAsync(userId)
+            ?? throw new KeyNotFoundException($"{paramName} with this ID not found.");
+    }
     public async Task<Message> SendMessageAsync(int senderId, int receiverId, string content)
     {
-        var sender = await _context.Users.FindAsync(senderId)
-            ?? throw new ArgumentException("Sender not found", nameof(senderId));
-        var receiver = await _context.Users.FindAsync(receiverId)
-            ?? throw new ArgumentException("Receiver not found", nameof(receiverId));
+        var sender = await FindUserOrThrowAsync(senderId, "Sender");
+        var receiver = await FindUserOrThrowAsync(receiverId, "Receiver");
+
+        if (string.IsNullOrWhiteSpace(content))
+        {
+            throw new ArgumentException("Message cannot be empty!", nameof(content));
+        }
 
         var message = new Message
         {
