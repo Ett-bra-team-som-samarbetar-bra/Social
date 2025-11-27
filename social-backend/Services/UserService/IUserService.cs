c226fd192e77b16c74e4f5f7e60fddd7ccc38e4c
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-11-27 10:57:04 +0100

    Setup userservice and userservicetests using tdd

---
+++ social-backend/Services/UserService/IUserService.cs
@@ -0,0 +1,7 @@
+public interface IUserService
+{
+    Task DeleteUser(UserIdRequest request);
+    Task<List<User>> GetAllUsers();
+    Task<User> GetUserById(UserIdRequest request);
+    Task UpdatePassword(UpdatePasswordRequest request);
+}
