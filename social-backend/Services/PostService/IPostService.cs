275d58bb65ad5384eeaa189a952070d918895896
Jesper <jw_jw@hotmail.com>
2025-11-27 09:29:29 +0100

    add correct interface for postservice

--- social-backend/Services/PostService/IPostService.cs
+++ social-backend/Services/PostService/IPostService.cs
@@ -6,4 +6,6 @@ public interface IPostService
     public Task<PaginatedList<Post>> GetPosts(int pageIndex, int pageSize);
     public Task<PaginatedList<Post>> GetUserPosts(int pageIndex, int pageSize, User user);
     public Task<PaginatedList<Post>> GetFollowingPosts(int pageIndex, int pageSize, User user);
+    public Task<Post> CreatePost(CreatePostDto dto);
+    public Task<Comment> CreateComment(CreateCommentDto dto, int postId);
 }
