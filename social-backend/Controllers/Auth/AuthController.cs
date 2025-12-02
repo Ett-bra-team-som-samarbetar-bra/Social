5ab5e1f4d1184412d21917ce14f7817faf35d921
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-12-02 16:40:26 +0100

    Fixed errors in controllers with path and http-calls

--- social-backend/Controllers/Auth/AuthController.cs
+++ social-backend/Controllers/Auth/AuthController.cs
@@ -1,24 +1,24 @@
 namespace social_backend.Controllers;
 
 [ApiController]
-[Route("api/auth/[controller]")]
+[Route("api/auth")]
 public class AuthController(IAuthService authService) : ControllerBase
 {
     private readonly IAuthService _authService = authService;
 
-    [HttpPost]
+    [HttpPost("login")]
     public async Task<ActionResult> Login(LoginRequest request)
     {
         await _authService.Login(request, HttpContext);
         return Ok("Log in successful");
     }
-
+    [HttpPost("register")]
     public async Task<ActionResult> Register(RegisterRequest request)
     {
         await _authService.RegisterAsync(request);
         return Ok("Register successful");
     }
-
+    [HttpGet("me")]
     public async Task<ActionResult<User>> Me()
     {
         var loggedInUser = await _authService.GetLoggedInUser(HttpContext);
