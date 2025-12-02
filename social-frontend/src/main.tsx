125f755feb1a9f11f87facb28caa261fd185ca95
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2025-12-02 11:34:49 +0100

    Added baseline for loginpage

--- social-frontend/src/main.tsx
+++ social-frontend/src/main.tsx
@@ -1,21 +1,24 @@
 import type { RouteObject } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "../sass/index.scss";
-import App from './App';
-import routes from './routes';
+import App from "./App";
+import routes from "./routes";
+import { AuthProvider } from "./Auth/AuthProvider";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: routes as RouteObject[]
+    children: routes as RouteObject[],
   },
 ]);
 
-createRoot(document.querySelector('#root')!).render(
+createRoot(document.querySelector("#root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
